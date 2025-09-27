458b2ca5bf9fbb72646b22bc7be3d5968cc9a7af
Mihaly Varga <mihaly.varga@elpro.com>
2025-09-27 15:42:36 +0200

    feat: Add controller and define endpoints

---
+++ UrlShortener.Presentation/Controllers/UrlController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Application.Services;
+
+namespace UrlShortener.Presentation.Controllers;
+
+[ApiController]
+[Route("")]
+public class UrlController(UrlShortenerService service) : ControllerBase
+{
+    [HttpPost("api/url/shorten")]
+    public ActionResult<string> Shorten([FromBody] string longUrl)
+    {
+        var code = service.ShortenUrl(longUrl);
+
+        var shortened = $"{Request.Scheme}://{Request.Host}/{code}";
+        return Ok(shortened);
+    }
+
+    [HttpGet("{shortCode}")]
+    public IActionResult RedirectToLongUrl(string shortCode)
+    {
+        try
+        {
+            var longUrl = service.ExpandUrl(shortCode);
+            return Redirect(longUrl);
+        }
+        catch (InvalidOperationException)
+        {
+            return NotFound("Short URL not found");
+        }
+    }
+}
