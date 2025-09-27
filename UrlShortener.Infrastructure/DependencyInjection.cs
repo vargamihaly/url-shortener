011d3ded54438eff1bda43354cd0c727d9a41660
Mihaly Varga <mihaly.varga@elpro.com>
2025-09-27 15:45:21 +0200

    chore: add infrastructure dependency injection configuration

---
+++ UrlShortener.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Domain.Interfaces;
+using UrlShortener.Infrastructure.Persistence;
+
+namespace UrlShortener.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<UrlMapContext>(options =>
+            options.UseInMemoryDatabase("UrlShortenerDb"));
+
+        services.AddScoped<IUrlMapDb, EfUrlMapDb>();
+        return services;
+    }
+}
